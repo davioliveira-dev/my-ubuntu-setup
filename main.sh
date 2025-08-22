78e7c7caad64485c5d0bcd68e7fdc64af4ddb5ea
Davi Oliveira <davioliveira.java@gmail.com>
2025-08-22 00:14:46 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -119,6 +119,7 @@ setup_dev_tools() {
     curl https://mise.run | sh
     echo 'eval "$(~/.local/bin/mise activate bash)"' >> ~/.bashrc
     ~/.local/bin/mise use --global node@22
+    source ~/.bashrc
     npm i -g npm pnpm bun
 
     # Opencoed
